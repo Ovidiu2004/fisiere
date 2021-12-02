ca2486a5a0b8901502efd1eadd501bc7f93473ec
Frecautanu Ovidiu <70950196+Ovidiu2004@users.noreply.github.com>
2021-12-02 19:30:42 +0300

    Add files via upload

--- problema 7 manual.py
+++ problema 7 manual.py
@@ -13,7 +13,7 @@ for i in range(0,len(cr)):
     if cr[i]=='a' or cr[i]=='i' or cr[i]=='e' or cr[i]=='o' or cr[i]=='u' or cr[i]=='A' or cr[i]=='E' or cr[i]=='I' or cr[i]=='U' or cr[i]=='O':
       v.extend(cr[i])
 
-f=open("C:\Informatica vocale.txt",'w')
+f=open("D:\Vocale.txt",'w')
 f.write(str(v))
 f.write("\n ")
 f.write(str(len(v)))
